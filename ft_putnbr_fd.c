9a13ee63b7c978542904b333212bc15205cfb545
Ali Choukeir <achoukei@c1r5s5.42beirut.com>
2025-11-19 21:19:50 +0200

    Fix

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: achoukei <achoukei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 14:57:51 by achoukei          #+#    #+#             */
-/*   Updated: 2025/11/12 16:47:01 by achoukei         ###   ########.fr       */
+/*   Updated: 2025/11/08 19:00:34 by achoukei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,26 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	char	temp;
 
-
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
 	if (n < 0)
 	{
 		write(fd, "-", 1);
 		n = -n;
 	}
-	if (n > 9 || n > -10)
+	if (n > 9)
 		ft_putnbr_fd((n / 10), fd);
 	temp = '0' + (n % 10);
 	write(fd, &temp, 1);
 }
 
-int	main(void)
-{
-	int	fd;
+// int	main(void)
+// {
+// 	int	fd;
 
-	ft_putnbr_fd(543, 1);
-}
+// 	fd = open("ft_putnbr_fd.c", O_RDWR);
+// 	ft_putnbr_fd(543, fd);
+// }
